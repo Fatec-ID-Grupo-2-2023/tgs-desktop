940b06789289ae3d95d18a1faba8e5ecd183a048
Caique <caiquepatelliescapeline@gmail.com>
2022-04-16 08:40:49 -0300

    create configs

---
+++ TGS/Model/Configs.cs
@@ -0,0 +1,16 @@
+﻿namespace TGS.Model {
+    public class Configs {
+        private static string dbRoute;
+        private static int[] reportsList;
+
+        public static string DBRoute {
+            get { return dbRoute; }
+            set { dbRoute = value; }
+        }
+
+        public static int[] ReportsList {
+            get { return reportsList; }
+            set { reportsList = value; }
+        }
+    }
+}
