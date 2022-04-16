3f6b9bc74bb20e7f6f62746631982ef05b403a04
Caique <caiquepatelliescapeline@gmail.com>
2022-04-16 08:40:25 -0300

    create database

---
+++ TGS/Model/DBConnection.cs
@@ -0,0 +1,26 @@
+﻿using System.Data.SqlClient;
+
+namespace TGS.Model {
+    class DBConnection {
+        
+        public DBConnection() {
+            dbConn.ConnectionString = @Configs.DBRoute;
+        }
+
+        // Classes
+        SqlConnection dbConn = new SqlConnection();
+
+        public SqlConnection Connect() {
+            if (dbConn.State == System.Data.ConnectionState.Closed) {
+                dbConn.Open();            
+            }
+            return dbConn;
+        }
+
+        public void Disconnect() {
+            if (dbConn.State == System.Data.ConnectionState.Open) {
+                dbConn.Close();
+            }
+        }
+    }
+}
---
+++ TGS/Model/Session.cs
@@ -0,0 +1,16 @@
+﻿namespace TGS.Model {
+    public class Session {
+        private static string name;
+        private static string cpf;
+
+        public static string Name {
+            get {return Session.name;}
+            set {Session.name = value;}
+        }
+
+        public static string CPF {
+            get {return Session.cpf;}
+            set {Session.cpf = value;}
+        }
+    }
+}
